9e94089f11257f5b6d50b5b70d894d535018ad68
YOUR_NAME <steve3341@DESKTOP-DEMPQ84>
2023-02-06 22:48:13 -0500

    updated 99

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,7 +1,13 @@
 function bottleSong() {
-  console.log("hello")
+  let result = []
+  for(let i = 99; i >= 3; i-- ){
+    result.push(`${i} bottles of beer on the wall, ${i} bottles of beer.
+    Take one down and pass it around, ${i-1} bottles of beer on the wall.`)}
+  
+    result.push("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.")
+ return result
   
   // code goes here
-};
-
-bottleSong();
+;
+}
+console.log(bottleSong())
